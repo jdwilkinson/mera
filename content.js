c98b89c8e583896a7fc0131f14ebdd2e69968343
jdwilkinson <jdwilkinson@gmail.com>
2017-10-03 20:52:25 -0700

    Remove all sections containing Kellerman

--- content.js
+++ content.js
@@ -1,1 +1,31 @@
-alert("You're on an ESPN page.  Hello from MERA.");
+var body = document.getElementsByTagName("body")[0];
+
+handleChildren(body);
+
+function handleChildren(node) {
+	var children = node.childNodes;
+	for (var i=0; i < children.length; i++) {
+		var child = children[i];
+		if (child.nodeValue && child.nodeValue.includes('Kellerman')) {
+			var section = findParent(child, "section");
+			var sectionParent = section.parentNode;
+			sectionParent.removeChild(section);
+			var span = document.createElement("section");
+			var text = document.createTextNode("ESPN is so reasonable!");
+			span.appendChild(text);
+			sectionParent.appendChild(span);
+		} else {
+			handleChildren(child);
+		}
+	}
+}
+
+function findParent(node, tagName) {
+	if (!node) {
+		return null;
+	}
+	if (node.tagName && node.tagName.toLowerCase() == tagName) {
+		return node;
+	}
+	return findParent(node.parentNode, tagName);
+}
